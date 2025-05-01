1c539db111e992bdb35590a09466983eafe8c213
Moshood Adeleye <adeleyemosh@gmail.com>
2025-05-01 11:51:14 +0100

    Initial commit

---
+++ .config/zshrc/10-flutter.zsh
@@ -0,0 +1,12 @@
+export HOME="/home/mosh"
+
+export PATH="$HOME/flutter/bin:$PATH"
+# export PATH="$PATH:/opt/flutter/bin"
+
+export CHROME_EXECUTABLE="/usr/bin/brave"
+
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
