6aa0d5069bb8d9ad9bda09aa29c36b299db08c9f
Moshood Adeleye <adeleyemosh@gmail.com>
2025-05-12 00:15:10 +0100

    Housekeeping

--- waybar/.config/waybar/themes/moshs-lio-v2/scripts/title/icon_check.sh
+++ waybar/.config/waybar/themes/moshs-lio-v2/scripts/title/icon_check.sh
@@ -1,10 +1,22 @@
-#!/usr/bin/env bash
-#
+#!/bin/bash
 
 app_name=$(hyprctl activewindow | grep -oP 'class: \K[^\s]+')
 
 if [ -z "$app_name" ]; then
-  echo ""
-else
-  echo "󪥒"
+  echo "No active window"
+  exit 0
 fi
+
+# Map app class to icon
+case "$app_name" in
+  "kitty") icon="" ;;
+  "Geany") icon="📝" ;;
+  "firefox") icon="" ;;
+  "code" | "Code") icon="󰨞" ;;
+  "discord") icon="󰙯" ;;
+  "thunar" | "nautilus") icon="" ;;
+  *) icon="" ;; # fallback icon
+esac
+
+echo "App: $app_name"
+echo "Icon: $icon"
